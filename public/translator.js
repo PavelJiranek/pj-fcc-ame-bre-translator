f9a4d21caa3f9d1fba363f2fc26e161cd0c4f6bc
Pavel Jiranek <jiranek35@gmail.com>
2020-09-21 17:41:13 +0200

    feat: translate titles WIP: non-case preserving

--- public/translator.js
+++ public/translator.js
@@ -3,6 +3,11 @@ import { britishOnly } from './british-only.js';
 import { americanToBritishSpelling } from './american-to-british-spelling.js';
 import { americanToBritishTitles } from './american-to-british-titles.js';
 
+const { invertObj } = R;
+
+const britishToAmericanTitles = invertObj(americanToBritishTitles);
+const britishToAmericanSpelling = invertObj(americanToBritishSpelling);
+
 const textArea = document.getElementById('text-input');
 const localeSelect = document.getElementById('locale-select');
 const translateButton = document.getElementById('translate-btn');
@@ -10,25 +15,92 @@ const clearButton = document.getElementById('clear-btn');
 const translationOutput = document.getElementById('translated-sentence');
 const errorContainer = document.getElementById('error-msg');
 
-const LOCALES = {
+const NO_TEXT_TO_TRANSLATE_ERROR = "Error: No text to translate.";
+const NO_TRANSLATION_NEEDED_MESSAGE = "Everything looks good to me!";
+
+const Locales = {
   "american-to-british": "american-to-british",
   "british-to-american": "british-to-american",
 }
-const NO_TEXT_TO_TRANSLATE_ERROR = "Error: No text to translate.";
-const NO_TRANSLATION_NEEDED_MESSAGE = "Everything looks good to me!";
+
+const DictionaryTypes = {
+  titles: 'titles',
+  spelling: 'spelling',
+  wordsAndPhrases: "wordsAndPhrases",
+}
+
+const Dictionaries = {
+  "american-to-british": {
+    titles: americanToBritishTitles,
+    spelling: americanToBritishSpelling,
+    wordsAndPhrases: americanOnly,
+  },
+  "british-to-american": {
+    titles: britishToAmericanTitles,
+    spelling: britishToAmericanSpelling,
+    wordsAndPhrases: britishOnly,
+  },
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   translateButton.onclick = handleTranslate;
   clearButton.onclick = handleClear;
 });
 
+const wrapTranslatedWord = word => `<span class="highlight">${word}</span>`;
+
+const translateByWord = (input, locale, dictionaryType) => {
+  const dictionary = Dictionaries[locale][dictionaryType];
+  let counter = 0;
+
+  const words = input.split(' ');
+  const translatedInput = words.map(word => {
+    const lcWord = word.toLowerCase();
+    const translation = dictionary[lcWord];
+    // todo preserve case, title tests
+    if (translation) {
+      counter++;
+      return wrapTranslatedWord(translation);
+    }
+    return word;
+  });
+
+  return [translatedInput.join(' '), counter];
+}
+
+const translateTitles = (input, locale) => {
+  return translateByWord(input, locale, DictionaryTypes.titles);
+}
+
+
 const handleTranslate = () => {
   if (!textArea.value) {
     translationOutput.textContent = ''
     errorContainer.textContent = NO_TEXT_TO_TRANSLATE_ERROR;
-  } else {
+    return;
+  }
+  errorContainer.textContent = '';
+
+  const locale = Locales[localeSelect.value],
+      input = textArea.value;
+  let translation = '',
+      translatedCounter = 0,
+      translations = [];
+
+  const updateTranslation = ([newTranslation, newCount]) => {
+    if (newCount) {
+      translation = newTranslation;
+      translatedCounter = newCount;
+    }
+  };
+
+  updateTranslation(translateTitles(input, locale));
+
+
+  if (translatedCounter === 0) {
     translationOutput.textContent = NO_TRANSLATION_NEEDED_MESSAGE;
-    errorContainer.textContent = '';
+  } else {
+    translationOutput.innerHTML = translation;
   }
 }
 
